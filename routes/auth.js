15d9846b7912d49fa08bd909243e7e94f57e43e3
adhith <24882134+kkadhith@users.noreply.github.com>
2023-01-05 17:20:12 -0800

    login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -21,6 +21,19 @@ router.post("/register", async (req, res) => {
     }
 });
 
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(404).json("user not found")
+        const validPass = await bcrypt.compare(req.body.password, user.password)
+        !validPass && res.status(400).json("wrong password")
+        res.status(200).json(user);
+    } catch (err){
+        console.log(err);
+    }
+});
+
+
 router.get("/", (req, res) => {
     res.send("auth");
 });
